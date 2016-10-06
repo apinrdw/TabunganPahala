95f0053b669677cd3780ca3f504fdf9c0f609c70
apinrdw <apinrdw@gmail.com>
2016-10-07 01:01:53 +0700

    added breadcrumbs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  semantic_breadcrumb :index, :root_path
+
   def current_period
     @current_period ||= Period.actives.first
   end
--- app/controllers/periods_controller.rb
+++ app/controllers/periods_controller.rb
@@ -1,14 +1,18 @@
 class PeriodsController < ApplicationController
+  semantic_breadcrumb :index, :periods_path
+
   def index
     @periods = Period.order(created_at: :desc)
   end
 
   def new
     @period = Period.new
+    semantic_breadcrumb :new, new_period_path
   end
 
   def create
     @period = Period.new(period_params)
+    semantic_breadcrumb :new, new_period_path
 
     if @period.save
       redirect_to periods_path, notice: 'Period was successfully created.'
