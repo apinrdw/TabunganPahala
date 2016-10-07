c1523852de882f240723adff14a960f41c24ca82
apinrdw <apinrdw@gmail.com>
2016-10-07 22:40:05 +0700

    checkout current period nil?

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @donations = current_period.donations.order(created_at: :desc)
+    @donations = current_period ? current_period.donations.order(created_at: :desc) : Donation.none
   end
 end
